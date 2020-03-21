89249cae2d379ee3d90cdc93d1c58e744f7dbc5c
Dzianis Hushcha <den.gushcha@gmail.com>
2020-03-21 18:37:11 +0300

    make succesfull conection to mysql server

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mega-secret-key'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/microblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
