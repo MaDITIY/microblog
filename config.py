707a624cf151c569215a6c211ee202a24a7d448b
Dzianis Hushcha <den.gushcha@gmail.com>
2020-03-28 16:46:43 +0300

    (Lesson 9) add pagination for messages on every view modify index, explore, user templates

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mega-secret-key'
+    POSTS_PER_PAGE = 10
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
