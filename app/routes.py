adcb4316ff0fe245a2457c3f0826acef69ab7153
MaDITIY <den.gushcha@gmail.com>
2023-04-10 19:32:15 +0300

    Add simple messages representation with dummy data.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 'Dzianis'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!',
+        },
+        {
+            'author': {'username': 'Viktoryia'},
+            'body': 'Let\'s have some fun!!!',
+        },
+        {
+            'author': {'username': 'Nick'},
+            'body': 'Hello world!!!',
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
