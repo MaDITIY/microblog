03a0194a5d7967cce5cf01502c1582c6285c7320
MaDITIY <den.gushcha@gmail.com>
2023-04-11 22:15:31 +0300

    Add login form and route to show it (w/o data handling yet).

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@
 from flask import render_template
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -23,3 +24,9 @@ def index():
         },
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
