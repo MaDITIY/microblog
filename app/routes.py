a0b660cb28f7c0728da624d49404f7c2efc56f02
MaDITIY <den.gushcha@gmail.com>
2023-04-12 21:00:11 +0300

    Add dummy form submit handling for login form.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from flask import flash
 from flask import render_template
+from flask import redirect
+from flask import url_for
 
 from app import app
 from app.forms import LoginForm
@@ -26,7 +29,12 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}'
+        )
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
