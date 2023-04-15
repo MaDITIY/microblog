4199c3fbbb4bf62d63f5b33600e518f8905f2baa
MaDITIY <den.gushcha@gmail.com>
2023-04-15 16:53:00 +0300

    Introduce DB Server with nescessary configs and new User mode.

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+sqlite_uri = f'sqlite:///{os.path.join(basedir, "app.db")}'
+postgres_db_name = 'microblog'
+postgres_uri = f'postgresql:///{postgres_db_name}'
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or postgres_uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
