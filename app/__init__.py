0fbb6dcaae0a5501bda465bbfa282349b11922a2
MaDITIY <den.gushcha@gmail.com>
2023-04-23 19:29:26 +0300

    Add logging to application.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
+import logging
+import os.path
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -12,5 +17,37 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 
 login.login_view = 'login'
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup.')
+
+    mail_server = app.config['MAIL_SERVER']  # Google no longer allows 'less secure apps' to use mail accounts directly
+    if mail_server:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(mail_server, app.config['MAIL_PORT']),
+            fromaddr=f'no-reply@{app.config["MAIL_SERVER"]}',
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Failure',
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 from app import errors, routes, models
