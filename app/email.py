e39294ee92377d8db15c03725e7722907fdb63a1
MaDITIY <den.gushcha@gmail.com>
2023-04-28 19:02:19 +0300

    Move send_mail to a separate thread.

--- app/email.py
+++ app/email.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
@@ -26,8 +28,14 @@ def send_password_reset_email(user):
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
-    """Send email."""
+    """Start a new thread to send email."""
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
+
+
+def send_async_email(app, msg):
+    """Send email."""
+    with app.app_context():
+        mail.send(msg)
