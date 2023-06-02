9b330e90d6e89e1403e95ba4d871228abce8fcff
MaDITIY <den.gushcha@gmail.com>
2023-06-02 17:50:59 +0300

    Enhance send mail function to be able to send attachments and send synchronos emails.

--- app/email.py
+++ app/email.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from typing import Iterable
 
 from flask import current_app
 from flask_mail import Message
@@ -6,15 +7,28 @@ from flask_mail import Message
 from app import mail
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(
+        subject: str,
+        sender: str,
+        recipients: list,
+        text_body: str,
+        html_body: str,
+        attachments: Iterable = tuple(),
+        send_async: bool = False,
+) -> None:
     """Start a new thread to send email."""
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(
-        target=send_async_email, 
-        args=(current_app._get_current_object(), msg)
-    ).start()
+    for attachment in attachments:
+        msg.attach(*attachment)
+    if send_async:
+        Thread(
+            target=send_async_email,
+            args=(current_app._get_current_object(), msg)
+        ).start()
+    else:
+        mail.send(msg)
 
 
 def send_async_email(app, msg):
