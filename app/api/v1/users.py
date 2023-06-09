34d4dc5781ccd172c13965a99c6d98ff2723687b
MaDITIY <den.gushcha@gmail.com>
2023-06-09 20:43:03 +0300

    Implement 'followers' and 'followed' user endpoints.

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -5,6 +5,9 @@ from app.api.v1 import bp
 from app.models import User
 
 
+MAX_COLLECTIONS_COUNT = 100
+
+
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
     """Get single user by ID."""
@@ -16,7 +19,7 @@ def get_user(id):
 def get_users():
     """Get all application users."""
     page = request.args.get('page', 1, type=int)
-    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    per_page = min(request.args.get('per_page', 10, type=int), MAX_COLLECTIONS_COUNT)
     data = User.to_collection_dict(
         query=User.query,
         page=page,
@@ -28,12 +31,26 @@ def get_users():
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
-    pass
+    """Get User followers by user ID."""
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), MAX_COLLECTIONS_COUNT)
+    data = User.to_collection_dict(
+        user.followers, page, per_page, 'api.get_followers', id=id
+    )
+    return jsonify(data)
 
 
 @bp.route('/users/<int:id>/followed', methods=['GET'])
 def get_followed(id):
-    pass
+    """Get User followed by user ID."""
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), MAX_COLLECTIONS_COUNT)
+    data = User.to_collection_dict(
+        user.followed, page, per_page, 'api.get_followed', id=id
+    )
+    return jsonify(data)
 
 
 @bp.route('/users', methods=['POST'])
