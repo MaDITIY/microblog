37041af0db9f259f3412f97b3cd1d5660f1434a2
MaDITIY <den.gushcha@gmail.com>
2023-06-10 15:39:35 +0300

    Add API basic auth.

--- app/api/v1/tokens.py
+++ app/api/v1/tokens.py
@@ -1,8 +1,18 @@
+from flask import g
+from flask import jsonify
+
+from app import db
 from app.api.v1 import bp
+from app.api.v1.auth import basic_auth
 
 
+@bp.route('/tokens', methods=['POST'])
+@basic_auth.login_required
 def get_token():
-    pass
+    """Get auth token."""
+    token = g.current_user.get_token()
+    db.session.commit()
+    return jsonify({'token': token})
 
 
 def revoke_token():
