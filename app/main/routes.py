c7328e417d8415599ef441083aab331c8945ccea
MaDITIY <den.gushcha@gmail.com>
2023-06-11 20:14:49 +0300

    Replace "Search" pagination with subtemplate.

--- app/main/routes.py
+++ app/main/routes.py
@@ -239,8 +239,13 @@ def search():
     prev_page_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
     return render_template(
-        'search.html', title=_('Search'), posts=posts, 
-        next_url=next_page_url, prev_url=prev_page_url
+        'search.html',
+        title=_('Search'),
+        posts=posts, 
+        next_url=next_page_url,
+        prev_url=prev_page_url,
+        next_page_title=_('Next results'),
+        prev_page_title=_('Previous results'),
     )
 
 
